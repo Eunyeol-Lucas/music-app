247f3930e16a5fc21474b2d7b39144d68fa6ca3d
Eunyeol <ney9083@gmail.com>
2022-05-04 12:33:07 +0900

    Style: api key 제외

--- app.js
+++
@@ -1,7 +0,0 @@
-import Controller from "./components/controller.js";
-import Albums from "./components/model.js";
-import SearchedAlbums from "./components/view.js";
-
-window.addEventListener("DOMContentLoaded", () => {
-  new Controller();
-});
--- utils/consts.js
+++
@@ -1,5 +0,0 @@
-export const API_KEY = "15765d1dbbb01e2baaef37788b01cf5f";
-export const URL = {
-  BASE_URL: "https://ws.audioscrobbler.com/2.0/",
-  SEARCH_URL: `?method=album.search&format=json`,
-};
